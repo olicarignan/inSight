2f270adcdc39205a937e6f3de2c8fbe47ae14e02
Olivier <olive.carignan@gmail.com>
2019-11-23 20:10:14 -0500

    fetch categories state with reducer function

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ function App() {
 
   const { state, dispatch } = useApplicationData();
 
+  console.log(state)
+
   const userList = state.users.map( user => (
     <li key={user.id}>
       {user.first_name} {user.last_name} {user.email}
     </li>
+  ))
+  const categoryList = state.categories.map(category => (
+    <li key={category.id}>
+      {category.category_name} {console.log(category)}
+      </li>
   ))
       
 
   return (
     <div className="App">
       <ul>{userList}</ul>
+      <ul>{categoryList}</ul>
       <div>
-        <Calendar></Calendar>
       </div>
     </div>
   );
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import dataReducer,{ SET_USERS } from '../reducers/dataReducer';
+import dataReducer,{ SET_APPLICATION_DATA } from '../reducers/dataReducer';
 
 export default function useApplicationData () {
 
-  const [state, dispatch] = useReducer(dataReducer, ({users: [], loading: true}));
+  const [state, dispatch] = useReducer(dataReducer, ({users: [], categories: [], loading: true}));
 
   useEffect(() => {
-    axios
-      .get('/api/users')
-      .then(result => { 
-        dispatch({ type: SET_USERS, users: result.data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
+    
+    const users = axios.get('/api/users');
+    const categories = axios.get('/api/categories');
+
+    Promise.all([users, categories])
+           .then(all => {
+             dispatch({
+               type: SET_APPLICATION_DATA,
+               users: all[0].data,
+               categories: all[1].data
+             })
+           })
+          }, []);
 
   return {
     state,
