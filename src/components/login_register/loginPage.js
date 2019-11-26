bb58673ca6aa5ddd8122b208ddf19e7761ba961d
Nasouh Al turk <nasouh@live.com>
2019-11-26 18:04:25 -0500

    working on the log in

--- src/components/login_register/loginPage.js
+++ src/components/login_register/loginPage.js
@@ -1,33 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import "./loginPage.scss"
+import './loginPage.scss';
+import axios from 'axios';
 
 export default function LoginPage(props) {
-  return(
-      <body>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-              <div className="card card-signin my-5">
-                <div className="card-body">
-                  <h5 className="card-title text-center">Sign In</h5>
-                  <form className="form-signin">
-                    <div className="form-label-group">
-                      <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus/>
-                      <label for="inputEmail">Email address</label>
-                    </div>
-                    <div className="form-label-group">
-                      <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
-                      <label for="inputPassword">Password</label>
-                    </div>
-                    <hr className="my-4"/>
-                    <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </body>
-  )
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+  function userLogin(event) {
+    event.preventDefault()
+    if(email && password) {
+      const currentUser = {
+        email,
+        password
+      }
+        console.log(currentUser)
+    }
+  }
+
+	return (
+		<body>
+			<div className="container">
+				<div className="row">
+					<div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+						<div className="card card-signin my-5">
+							<div className="card-body">
+								<h5 className="card-title text-center">Sign In</h5>
+								<form className="form-signin" onSubmit={(event) => userLogin(event)}>
+									<div className="form-label-group">
+										<input
+											type="email"
+											id="inputEmail"
+											className="form-control"
+											value={email}
+											onChange={(event) => {
+												setEmail(event.target.value);
+											}}
+											placeholder="Email address"
+											required
+											autofocus
+										/>
+										<label for="inputEmail">Email address</label>
+									</div>
+									<div className="form-label-group">
+										<input
+											type="password"
+											id="inputPassword"
+											value={password}
+											onChange={(event) => {
+												setPassword(event.target.value);
+											}}
+											className="form-control"
+											placeholder="Password"
+											required
+										/>
+										<label for="inputPassword">Password</label>
+									</div>
+									<hr className="my-4" />
+									<button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">
+										Sign in
+									</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+	);
 }
