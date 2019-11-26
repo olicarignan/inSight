c3dd4778f7763cb933510c599489430139586687
Olivier <olive.carignan@gmail.com>
2019-11-26 17:58:22 -0500

    usevisualmode

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import SideBar from './components/sidebar/sidebar';
 import LoginPage from './components/login_register/loginPage'
 import RegisterPage from './components/login_register/registerPage'
 import NotesList from './components/categoryPage/notesList';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
@@ -20,8 +26,16 @@ function App() {
     </li>
   ))
 
+
   return (
-    <div className="App">
+    <Router>
+    <Switch>
+      <Route exact path="/">
+        <LoginPage
+        users={state.users} />
+      </Route>
+      <Route path="/main">
+      <div className="App">
       <Nav
       users= {userList}
        />
@@ -30,12 +44,18 @@ function App() {
         categories={state.categories}
         />
         <div className="calendar-div">
-          {/* <Calendar /> */}
-          <NotesList
-          notes={state.notes} />
+          <Calendar />
         </div>
       </div>
     </div>
+      </Route>
+      <Route path="/category">
+        <Nav/>
+      <NotesList
+          notes={state.notes} />
+      </Route>
+      </Switch>
+    </Router>
     
   );
 }
