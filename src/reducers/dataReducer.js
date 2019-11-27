f50a4a98cdeb649246c162b654b54c983f2d4587
Olivier <olive.carignan@gmail.com>
2019-11-27 18:39:54 -0500

    giant reducer of hell

--- src/reducers/dataReducer.js
+++ src/reducers/dataReducer.js
@@ -1,39 +1,53 @@
 import React, {useReducer} from 'react';
 
-export const SET_USER = 'SET_USER';
 export const SET_APPLICATION_DATA = 'SET_APPLICATION_DATA';
-export const SET_USER_DATA = 'SET_USER_DATA'
+export const SET_LOGIN = 'SET_LOGIN';
+export const SET_LOGOUT = 'SET_LOGOUT';
+export const SET_USER = 'SET_USER';
+export const SET_USER_DATA = 'SET_USER_DATA';
+
 
 export default function dataReducer (state, action)  {
 
-  const { user, users, categories, appointments, notes } = action;
-
-  // switch(action.type) {
-  //   case SET_APPLICATION_DATA :
-  //     return ;
-  //   case SET_USER :
-  //     return ;
-  //   default :
-  //     return state;
-  // }
-
-  const actions = {
-    SET_APPLICATION_DATA: { ...state,
-      users: [ ...state.users, ...action.users],
-      categories: [ ...state.categories, ...action.categories],
-      appointments: [...state.appointments, ...action.appointments],
-      notes: [...state.notes, ...action.notes],
-      loading: false
-    },
-    SET_USER_DATA: { 
-      ...state,
-      user: user ? {...users} : null
-
-    }
+  switch (action.type) {
+    case SET_APPLICATION_DATA:
+      return {
+        ...state,
+        categories: [...action.categories],
+        appointments: [...action.appointments],
+        notes: [...action.notes],
+      }
+    case SET_LOGIN:
+      localStorage.setItem('token', action.token);
+      return {
+        ...state,
+        token: action.token,
+        isAuthenticated: true,
+        loading: false
+      };
+    case SET_LOGOUT:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false
+      };
+    case SET_USER_DATA: 
+       return { 
+       ...state,
+       user: action.user ? {...action.users} : null
+    };
+    case SET_USER:
+      return {
+        ...state,
+        token: action.token,
+        user: action.user,
+        isAuthenticated: true,
+        loading: false
+      };
+    default:
+      console.log("Unkown type in reducer");
+      return state;
   }
-  if (!actions[action.type]) {
-    throw new Error(`reducer type doesn't exist: ${action.type}`);
-  }
-  
-  return actions[action.type]
 }
