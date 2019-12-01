5de2debed26ed78a0eca6302562443fcab8285e1
Nasouh Al turk <nasouh@live.com>
2019-12-01 15:44:09 -0500

    wtrying to resolv bugs with meregs

--- src/components/textEditor/Editor.js
+++ src/components/textEditor/Editor.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { EDITOR_JS_TOOLS } from './editor-tools.js';
 import EditorJs from 'react-editor-js';
@@ -8,26 +7,23 @@ import './newText.scss';
 
 export default function Editor(props) {
 
-const  [ text, setText ] = useState(" ")
+	props.setNavButton('back to notes');
 
-  props.setNavButton('back to notes')
-  console.log(props)
+	console.log(props);
 
-  return(
-    <main>
-    <Nav
-    note={props.note}
-    user={props.user}
-    navButton={props.navButton}
-    />
-    <form className="text-editor" onSubmit={(event) => event.preventDefault() }>
-    
-    <Dante content={null}  />;
-     
-      <button className="text-editor-button" onClick={() => console.log("save")}> Save </button>
-      <button className="text-editor-button-delete btn-danger" onClick={() => console.log("delete")} >Delete</button>
-    </form>
-    </main>
-  )
+	return (
+		<main>
+			<Nav note={props.note} user={props.user} navButton={props.navButton} />
+			<form className="text-editor" onSubmit={(event) => event.preventDefault()}>
+				<Dante content={null} />;
+				<button className="text-editor-button" onClick={() => console.log('save')}>
+					{' '}
+					Save{' '}
+				</button>
+				<button className="text-editor-button-delete btn-danger" onClick={() => console.log('delete')}>
+					Delete
+				</button>
+			</form>
+		</main>
+	);
 }
-
