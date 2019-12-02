c77a6c81ce16906b4e594150ba603d7da79e7211
Nasouh Al turk <nasouh@live.com>
2019-12-02 18:01:44 -0500

    did some work on notifications

--- src/index.js
+++ src/index.js
@@ -6,7 +6,38 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+console.log('js is working')
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister()
+
+const check = () => {
+  if (!('serviceWorker' in navigator)) {
+    throw new Error('No Service Worker support!')
+  }
+  if (!('PushManager' in window)) {
+    throw new Error('No Push API Support!')
+  }
+}
+const registerServiceWorker = async () => {
+  const swRegistration = await navigator.serviceWorker.register('service.js')
+  return swRegistration
+}
+const requestNotificationPermission = async () => {
+  const permission = await window.Notification.requestPermission()
+  // value of permission can be 'granted', 'default', 'denied'
+  // granted: user has accepted the request
+  // default: user has dismissed the notification permission popup by clicking on x
+  // denied: user has denied the request.
+  if (permission !== 'granted') {
+    throw new Error('Permission not granted for Notification')
+  }
+}
+const main = async () => {
+  check()
+  const swRegistration = await registerServiceWorker()
+  const permission = await requestNotificationPermission()
+}
+main();
